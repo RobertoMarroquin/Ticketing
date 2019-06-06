a14c303c63811c097f03eb1ecf84c0283234222e
ChristianSurio <christiansurio404@gmail.com>
2019-06-06 13:09:27 -0600

    creacion de modelo "Golosina" sin atributos

--- Dulceria/models.py
+++ Dulceria/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Golosina(models.Model):
+    # TODO
+
+    class Meta:
+        abstract = True
+    
+    def __str__(self):
+        return self # TODO
